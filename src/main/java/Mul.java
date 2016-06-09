28f80b3270621718384cb50b17417230ff041d37
sowmya <prakashoct@gmail.com>
2016-06-09 21:25:44 +0530

    commited job3 file

---
+++ src/main/java/Mul.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Mul
+{
+public static void main(String[] args)
+{
+int a = 10;
+int b = 20;
+int mul = a*b;
+
+System.out.println("Sum :" +mul);
+
+}
+}
---
+++ src/main/java/Sub.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Sub
+{
+public static void main(String[] args)
+{
+int a = 10;
+int b = 20;
+int sub = a-b;
+
+System.out.println("Sum :" +sub);
+
+}
+}
